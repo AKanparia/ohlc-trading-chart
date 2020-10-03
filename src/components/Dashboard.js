40b67734fb306899c95dec0396a3f68ff8031b3e
Abhishek Kanparia <kanpariaabhishek@gmail.com>
2020-10-03 22:49:24 +0530

    Add chart to Historical overview screen

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,9 +2,11 @@ import React from 'react'
 
 function Dashboard({ title, children }) {
   return (
-    <main className='container'>
-      <h1>{title}</h1>
-      {children}
+    <main className='container dashboard'>
+      <div className='row'>
+        <h3 className='dashboard-title'>{title}</h3>
+        <div className='dashboard-content'>{children}</div>
+      </div>
     </main>
   )
 }
--- src/features/HistoricalView.js
+++ src/features/HistoricalView.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import { Dashboard } from '../components'
+import { Dashboard, ChartWithFeatures } from '../components'
 
 function HistoricalView({ data, isLoading, isError }) {
   return (
     <Dashboard title='Historical Overview'>
-      <pre>{JSON.stringify(data, undefined, 2)}</pre>
+      <ChartWithFeatures dataStream={data} />
     </Dashboard>
   )
 }
